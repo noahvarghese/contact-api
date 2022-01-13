297687f80dca4785055e22503d1fd9c421526bf2
noahvarghese <varghese.noah@gmail.com>
2022-01-13 19:31:23 +0000

    updated fields

--- pkg/getter/template.go
+++ pkg/getter/template.go
@@ -1,17 +1,22 @@
 package getter
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Template struct {
 	gorm.Model
-	Id int
-	Template string
-	Host_id int
+	ID        uint
+	Template  string
+	Host_id   uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
 }
 
-func (t *Template) Read(db *gorm.DB, h *Host) error {
-	tx := db.Unscoped().First(&t, "host_id = ?", h.Id)
+func (t *Template) Read(db *gorm.DB, id uint) error {
+	tx := db.First(&t, "host_id = ?", id)
 	return tx.Error
 }
