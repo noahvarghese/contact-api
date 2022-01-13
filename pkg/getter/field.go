79b9ee95db7f95b5af5afad3906ee0ec2ec9578e
noahvarghese <varghese.noah@gmail.com>
2022-01-13 19:32:36 +0000

    updated fields' fields and added method to get all for host

--- pkg/getter/field.go
+++ pkg/getter/field.go
@@ -1,18 +1,31 @@
 package getter
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Field struct {
 	gorm.Model
-	Id      int
-	Name    string
-	Type    string
-	Host_id int
+	ID        int
+	Name      string
+	Required  bool
+	Host_id   int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
 }
 
 func (f *Field) Read(db *gorm.DB, host_id int) error {
-	tx := db.Unscoped().First(&f, "host_id = ?", host_id)
+	tx := db.First(&f, "host_id = ?", host_id)
 	return tx.Error
+}
+
+func (f *Field) GetAll(db *gorm.DB, host_id uint) ([]Field, error) {
+	var fields []Field
+
+	tx := db.Where("host_id = ?", host_id).Find(&fields)
+
+	return fields, tx.Error
 }
