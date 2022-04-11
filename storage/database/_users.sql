7732ca5699f78d5cceeab58ff20b53211f911178
noahvarghese <varghese.noah@gmail.com>
2022-04-11 15:53:11 +0000

    updated sql user

--- storage/database/_users.sql
+++ storage/database/_users.sql
@@ -1,9 +1,10 @@
-CREATE USER 'contact'@'%' IDENTIFIED BY ;
+CREATE USER 'contact'@'%' IDENTIFIED BY 'tmp_password';
 /* DROP USER 'contact'; */
 
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'contact'@'%';
 
 GRANT SELECT ON contact.* TO 'contact'@'%';
 GRANT INSERT ON contact.messages TO 'contact'@'%';
+GRANT UPDATE (sent) ON contact.messages TO 'contact'@'%';
 
 FLUSH PRIVILEGES;
